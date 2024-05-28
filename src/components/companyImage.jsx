72a3fe8538d5cb3c9439712a9b3c8580f517bcb1
DaviiJones <davonbridgett@pursuit.org>
2024-05-28 03:29:02 -0400

    netlify man

--- src/components/companyImage.jsx
+++ src/components/companyImage.jsx
@@ -1,4 +1,4 @@
-import stackPaper from "/Users/davonbridgett/Documents/Munder-Difflin/public/e491b76e-e126-4896-aaf1-e40584b9125c_text.gif"; 
+import stackPaper from "../../public/e491b76e-e126-4896-aaf1-e40584b9125c_text.gif"; 
 import React from "react";
 
 const CompanyImage = () => {
