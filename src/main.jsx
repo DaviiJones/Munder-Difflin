0959af55e34015e83eda3737579e05925d255982
DaviiJones <davonbridgett@pursuit.org>
2024-05-28 03:24:33 -0400

    netlify build

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from '/Users/davonbridgett/Documents/Munder-Difflin/src/pages/Home.jsx'
+import Home from '../src/pages/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
